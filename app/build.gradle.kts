5307086cced4a8c78806d00d5cb7a4b78d06064d
Mickael MAGNIEZ <mickaelmagniez@gmail.com>
2026-09-23 08:43:00 +0200

    Release 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "io.github.mickaelmagniez.windbubble"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
